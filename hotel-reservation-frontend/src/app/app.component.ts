460f064a5821b862cdc389dbbd2ea537101433ef
shrey <allrooted1@gmail.com>
2025-02-28 11:26:47 +0530

    add buttons and booking form components with basic templates and styles

--- hotel-reservation-frontend/src/app/app.component.ts
+++ hotel-reservation-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookingTableComponent } from './booking-table/booking-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BookingTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
